1f75238e471ccc26d086303067d4318513529a6b
ilmmatias <93882972+yuuma03@users.noreply.github.com>
2023-08-23 16:24:54 -0300

    added a hello world in the kernel KASLR and everything else seems to be working

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,6 +1,8 @@
 /* SPDX-FileCopyrightText: (C) 2023 ilmmatias
  * SPDX-License-Identifier: BSD-3-Clause */
 
+#include <stdint.h>
+
 /*-------------------------------------------------------------------------------------------------
  * PURPOSE:
  *     This function is the kernel architecture-independent entry point.
@@ -12,6 +14,13 @@
  *     Does not return.
  *-----------------------------------------------------------------------------------------------*/
 [[noreturn]] void KernelMain(void) {
+    const char *Message = "Hello, World!";
+    uint16_t *Screen = (uint16_t *)0xFFFF8000000B8000;
+
+    for (int i = 0; Message[i]; i++) {
+        Screen[i] = Message[i] | 0x0700;
+    }
+
     while (1)
         ;
 }
