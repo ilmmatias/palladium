7ff1f7e7456d960397aa55fa4e518f8dae29a7a4
ilmmatias <93882972+ilmmatias@users.noreply.github.com>
2025-01-09 18:12:03 -0300

    Disable --dependency-file; Newer versions of CMake seems to enable that automatically, but it breaks building PE files

--- src/sdk/cmake/config.cmake
+++ src/sdk/cmake/config.cmake
@@ -21,9 +21,8 @@ set(CMAKE_CXX_STANDARD 23)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror")
+set(CMAKE_RC_FLAGS "--target=${TARGET_${ARCH}}-w64-mingw32")
 
 set(CMAKE_LINK_DEF_FILE_FLAG "")
-
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 add_link_options(-nostdlib -nodefaultlibs)
--- src/sdk/cmake/toolchain.cmake
+++ src/sdk/cmake/toolchain.cmake
@@ -5,12 +5,14 @@ set(CMAKE_SYSTEM_NAME Palladium)
 set(CMAKE_SYSTEM_PROCESSOR ${ARCH})
 
 set(CMAKE_C_COMPILER clang)
-set(CMAKE_C_COMPILER_WORKS 1)
-set(CMAKE_CXX_COMPILER clang)
-set(CMAKE_CXX_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_ASM_COMPILER clang)
+set(CMAKE_RC_COMPILER llvm-windres)
 set(CMAKE_AR llvm-ar)
 
-# We're missing a message compiler for now (probably just build the one from binutils?)
-set(CMAKE_RC_COMPILER llvm-windres)
-set(CMAKE_RC_FLAGS --target=${TARGET_${ARCH}}-w64-mingw32)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+set(CMAKE_C_COMPILER_WORKS 1)
+
+# Disable --dependency-file, it's not supported for .exe (PE) targets
+set(CMAKE_C_LINKER_DEPFILE_SUPPORTED FALSE)
+set(CMAKE_CXX_LINKER_DEPFILE_SUPPORTED FALSE)
