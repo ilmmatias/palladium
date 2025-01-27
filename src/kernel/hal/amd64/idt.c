716348e7ae4ae3d09bc2de7269f69de4fd44c0a6
ilmmatias <93882972+ilmmatias@users.noreply.github.com>
2025-01-26 21:12:49 -0300

    Dispatch and try to handle hardware exceptions before falling back to KeFatalError.

--- src/kernel/hal/amd64/idt.c
+++ src/kernel/hal/amd64/idt.c
@@ -2,16 +2,39 @@
  * SPDX-License-Identifier: GPL-3.0-or-later */
 
 #include <amd64/halp.h>
+#include <rt/except.h>
+#include <string.h>
 
 extern struct __attribute__((packed)) {
     char PushOp[2];
     char JumpOp[5];
 } HalpDefaultInterruptHandlers[224];
 
-extern void HalpDefaultTrapEntryWithoutErrorCode(void);
-extern void HalpDefaultTrapEntryWithErrorCode(void);
-extern void HalpPageFaultTrapEntry(void);
+extern void HalpDivisionTrapEntry(void);
+extern void HalpDebugTrapEntry(void);
 extern void HalpNmiTrapEntry(void);
+extern void HalpBreakpointTrapEntry(void);
+extern void HalpOverflowTrapEntry(void);
+extern void HalpBoundRangeTrapEntry(void);
+extern void HalpInvalidOpcodeTrapEntry(void);
+extern void HalpNumericCoprocessorTrapEntry(void);
+extern void HalpDoubleFaultTrapEntry(void);
+extern void HalpSegmentOverrunTrapEntry(void);
+extern void HalpInvalidTssTrapEntry(void);
+extern void HalpSegmentNotPresentTrapEntry(void);
+extern void HalpStackSegmentTrapEntry(void);
+extern void HalpGeneralProtectionTrapEntry(void);
+extern void HalpPageFaultTrapEntry(void);
+extern void HalpReservedTrapEntry(void);
+extern void HalpX87FloatingPointTrapEntry(void);
+extern void HalpAlignmentCheckTrapEntry(void);
+extern void HalpMachineCheckTrapEntry(void);
+extern void HalpSimdFloatingPointTrapEntry(void);
+extern void HalpVirtualizationTrapEntry(void);
+extern void HalpControlProtectionTrapEntry(void);
+extern void HalpHypervisorInjectionTrapEntry(void);
+extern void HalpVmmCommunicationTrapEntry(void);
+extern void HalpSecurityTrapEntry(void);
 extern void HalpDispatchEntry(void);
 extern void HalpTimerEntry(void);
 
@@ -20,38 +43,38 @@ static struct {
     uint8_t Ist;
     uint8_t Dpl;
 } TrapEntries[32] = {
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpDivisionTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpDebugTrapEntry, DESCR_IST_NONE, DESCR_DPL_USER},
     {HalpNmiTrapEntry, DESCR_IST_NMI, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_USER},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_DOUBLE_FAULT, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpBreakpointTrapEntry, DESCR_IST_NONE, DESCR_DPL_USER},
+    {HalpOverflowTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpBoundRangeTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpInvalidOpcodeTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpNumericCoprocessorTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpDoubleFaultTrapEntry, DESCR_IST_DOUBLE_FAULT, DESCR_DPL_KERNEL},
+    {HalpSegmentOverrunTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpInvalidTssTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpSegmentNotPresentTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpStackSegmentTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpGeneralProtectionTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
     {HalpPageFaultTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_MACHINE_CHECK, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
-    {HalpDefaultTrapEntryWithoutErrorCode, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpX87FloatingPointTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpAlignmentCheckTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpMachineCheckTrapEntry, DESCR_IST_MACHINE_CHECK, DESCR_DPL_KERNEL},
+    {HalpSimdFloatingPointTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpVirtualizationTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpControlProtectionTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpHypervisorInjectionTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpVmmCommunicationTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpSecurityTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
+    {HalpReservedTrapEntry, DESCR_IST_NONE, DESCR_DPL_KERNEL},
 };
 
 /*-------------------------------------------------------------------------------------------------
@@ -64,7 +87,7 @@ static struct {
  * RETURN VALUE:
  *     None.
  *-----------------------------------------------------------------------------------------------*/
-void HalpNmiHandler(HalInterruptFrame *) {
+void HalpDispatchNmi(HalInterruptFrame *) {
     if (HalGetCurrentProcessor()->EventStatus == KE_EVENT_FREEZE) {
         while (1) {
             HalpStopProcessor();
@@ -72,6 +95,130 @@ void HalpNmiHandler(HalInterruptFrame *) {
     }
 
     KeFatalError(KE_PANIC_NMI_HARDWARE_FAILURE);
+}
+
+/*-------------------------------------------------------------------------------------------------
+ * PURPOSE:
+ *     This function tries gracefully handling an exception (and continuing execution.
+ *
+ * PARAMETERS:
+ *     ExceptionCode - Type of the exception.
+ *     InterruptFrame - Volatile register data.
+ *     ExceptionFrame - Non-volatile register data.
+ *
+ * RETURN VALUE:
+ *     None.
+ *-----------------------------------------------------------------------------------------------*/
+void HalpDispatchException(
+    uint32_t ExceptionCode,
+    HalInterruptFrame *InterruptFrame,
+    HalExceptionFrame *ExceptionFrame) {
+    /* FLT_INVALID_OPERATION is our "magic number" to indicate we need to expand which floating
+     * point fault happend. TODO: There is a bit more of handling (of other exceptions) we need to
+     * do here too. */
+    if (ExceptionCode == RT_EXC_FLT_INVALID_OPERATION) {
+        if (InterruptFrame->Mxcsr & 0x01) {
+            ExceptionCode = RT_EXC_FLT_INVALID_OPERATION;
+        } else if (InterruptFrame->Mxcsr & 0x02) {
+            ExceptionCode = RT_EXC_FLT_DENORMAL_OPERAND;
+        } else if (InterruptFrame->Mxcsr & 0x04) {
+            ExceptionCode = RT_EXC_FLT_DIVIDE_BY_ZERO;
+        } else if (InterruptFrame->Mxcsr & 0x08) {
+            ExceptionCode = RT_EXC_FLT_OVERFLOW;
+        } else if (InterruptFrame->Mxcsr & 0x10) {
+            ExceptionCode = RT_EXC_FLT_UNDERFLOW;
+        } else if (InterruptFrame->Mxcsr & 0x20) {
+            ExceptionCode = RT_EXC_FLT_INEXACT_RESULT;
+        }
+    }
+
+    /* Now build the exception record. */
+    RtExceptionRecord ExceptionRecord;
+    memset(&ExceptionRecord, 0, sizeof(RtExceptionRecord));
+    ExceptionRecord.ExceptionCode = ExceptionCode;
+    ExceptionRecord.ExceptionFlags = 0;
+    ExceptionRecord.ExceptionRecord = NULL;
+    ExceptionRecord.ExceptionAddress = (void *)InterruptFrame->Rip;
+    ExceptionRecord.NumberOfParameters = 0;
+
+    /* And the unwind context (using both frames). */
+    RtContext ContextRecord;
+    ContextRecord.Rax = InterruptFrame->Rax;
+    ContextRecord.Rcx = InterruptFrame->Rcx;
+    ContextRecord.Rdx = InterruptFrame->Rdx;
+    ContextRecord.Rbx = ExceptionFrame->Rbx;
+    ContextRecord.Rsp = InterruptFrame->Rsp;
+    ContextRecord.Rbp = ExceptionFrame->Rbp;
+    ContextRecord.Rsi = ExceptionFrame->Rsi;
+    ContextRecord.Rdi = ExceptionFrame->Rdi;
+    ContextRecord.R8 = InterruptFrame->R8;
+    ContextRecord.R9 = InterruptFrame->R9;
+    ContextRecord.R10 = InterruptFrame->R10;
+    ContextRecord.R11 = InterruptFrame->R11;
+    ContextRecord.R12 = ExceptionFrame->R12;
+    ContextRecord.R13 = ExceptionFrame->R13;
+    ContextRecord.R14 = ExceptionFrame->R14;
+    ContextRecord.R15 = ExceptionFrame->R15;
+    ContextRecord.Xmm0 = InterruptFrame->Xmm0;
+    ContextRecord.Xmm1 = InterruptFrame->Xmm1;
+    ContextRecord.Xmm2 = InterruptFrame->Xmm2;
+    ContextRecord.Xmm3 = InterruptFrame->Xmm3;
+    ContextRecord.Xmm4 = InterruptFrame->Xmm4;
+    ContextRecord.Xmm5 = InterruptFrame->Xmm5;
+    ContextRecord.Xmm6 = ExceptionFrame->Xmm6;
+    ContextRecord.Xmm7 = ExceptionFrame->Xmm7;
+    ContextRecord.Xmm8 = ExceptionFrame->Xmm8;
+    ContextRecord.Xmm9 = ExceptionFrame->Xmm9;
+    ContextRecord.Xmm10 = ExceptionFrame->Xmm10;
+    ContextRecord.Xmm11 = ExceptionFrame->Xmm11;
+    ContextRecord.Xmm12 = ExceptionFrame->Xmm12;
+    ContextRecord.Xmm13 = ExceptionFrame->Xmm13;
+    ContextRecord.Xmm14 = ExceptionFrame->Xmm14;
+    ContextRecord.Xmm15 = ExceptionFrame->Xmm15;
+    ContextRecord.Rip = InterruptFrame->Rip;
+    ContextRecord.Rflags = InterruptFrame->Rflags;
+
+    /* If we fail to dispatch, we're just falling back to panicking. */
+    if (!RtDispatchException(&ExceptionRecord, &ContextRecord)) {
+        KeFatalError(KE_PANIC_TRAP_NOT_HANDLED);
+    }
+
+    /* Otherwise, we gotta do the opposite now (rebuild our interrupt and exception frames using the
+     * context record). */
+    InterruptFrame->Rax = ContextRecord.Rax;
+    InterruptFrame->Rcx = ContextRecord.Rcx;
+    InterruptFrame->Rdx = ContextRecord.Rdx;
+    ExceptionFrame->Rbx = ContextRecord.Rbx;
+    InterruptFrame->Rsp = ContextRecord.Rsp;
+    ExceptionFrame->Rbp = ContextRecord.Rbp;
+    ExceptionFrame->Rsi = ContextRecord.Rsi;
+    ExceptionFrame->Rdi = ContextRecord.Rdi;
+    InterruptFrame->R8 = ContextRecord.R8;
+    InterruptFrame->R9 = ContextRecord.R9;
+    InterruptFrame->R10 = ContextRecord.R10;
+    InterruptFrame->R11 = ContextRecord.R11;
+    ExceptionFrame->R12 = ContextRecord.R12;
+    ExceptionFrame->R13 = ContextRecord.R13;
+    ExceptionFrame->R14 = ContextRecord.R14;
+    ExceptionFrame->R15 = ContextRecord.R15;
+    InterruptFrame->Xmm0 = ContextRecord.Xmm0;
+    InterruptFrame->Xmm1 = ContextRecord.Xmm1;
+    InterruptFrame->Xmm2 = ContextRecord.Xmm2;
+    InterruptFrame->Xmm3 = ContextRecord.Xmm3;
+    InterruptFrame->Xmm4 = ContextRecord.Xmm4;
+    InterruptFrame->Xmm5 = ContextRecord.Xmm5;
+    ExceptionFrame->Xmm6 = ContextRecord.Xmm6;
+    ExceptionFrame->Xmm7 = ContextRecord.Xmm7;
+    ExceptionFrame->Xmm8 = ContextRecord.Xmm8;
+    ExceptionFrame->Xmm9 = ContextRecord.Xmm9;
+    ExceptionFrame->Xmm10 = ContextRecord.Xmm10;
+    ExceptionFrame->Xmm11 = ContextRecord.Xmm11;
+    ExceptionFrame->Xmm12 = ContextRecord.Xmm12;
+    ExceptionFrame->Xmm13 = ContextRecord.Xmm13;
+    ExceptionFrame->Xmm14 = ContextRecord.Xmm14;
+    ExceptionFrame->Xmm15 = ContextRecord.Xmm15;
+    InterruptFrame->Rip = ContextRecord.Rip;
+    InterruptFrame->Rflags = ContextRecord.Rflags;
 }
 
 /*-------------------------------------------------------------------------------------------------
@@ -178,7 +325,7 @@ void HalpInitializeIdt(KeProcessor *Processor) {
             Base = (uint64_t)&HalpDefaultInterruptHandlers[i - 32];
         }
 
-        InitializeEntry(Processor, i, Base, DESCR_SEG_KCODE, Ist, IDT_TYPE_INT, DESCR_DPL_KERNEL);
+        InitializeEntry(Processor, i, Base, DESCR_SEG_KCODE, Ist, IDT_TYPE_INT, Dpl);
         RtInitializeDList(&Processor->InterruptList[i]);
     }
 
