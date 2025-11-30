2da4e39e997b0412fc3dc87e08045c51f5dd46c7
ilmmatias <93882972+ilmmatias@users.noreply.github.com>
2025-11-30 09:22:39 -0300

    Add in a memset_explicit implementation to the CRT (just call memset; maybe we should somehow inline the memset inside the function instead?)

---
+++ src/sdk/crt/string/memset_explicit.c
@@ -0,0 +1,25 @@
+/* SPDX-FileCopyrightText: (C) 2023-2025 ilmmatias
+ * SPDX-License-Identifier: GPL-3.0-or-later */
+
+#include <string.h>
+
+/*-------------------------------------------------------------------------------------------------
+ * PURPOSE:
+ *     This function implements code to fill a region of memory with a specific byte, in a way
+ *     that can be used to clear sensitive regions of memory.
+ *
+ * PARAMETERS:
+ *     s - Destination buffer.
+ *     c - Byte to be used as pattern.
+ *     n - Size of the destination buffer.
+ *
+ * RETURN VALUE:
+ *     Start of the destination buffer.
+ *-----------------------------------------------------------------------------------------------*/
+void *memset_explicit(void *s, int c, size_t n) {
+    /* I think doing a compiler barrier like this should safe (as the actual asm string is
+     * empty)? */
+    memset(s, c, n);
+    __asm__ volatile("" : : "g"(&s) : "memory");
+    return s;
+}
